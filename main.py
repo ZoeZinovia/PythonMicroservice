456ba07329748f624eb80d47635cb56405e8c0a7
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-03-24 15:56:16 +0000

    Adding adafruit temp and humidity

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from time import sleep # Import the sleep function from the time module
+import Adafruit_DHT
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+sensor = 22 # might need to be changed depending on the pi setup
+pin = 12 # might need to be changed depending on the pi setup
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-while True: # Run forever
- GPIO.output(8, GPIO.HIGH) # Turn on
- sleep(1) # Sleep for 1 second
- GPIO.output(8, GPIO.LOW) # Turn off
- sleep(1) # Sleep for 1 second
+print(humidity)
+print(temperature)
+# GPIO.setwarnings(False) # Ignore warning for now
+# GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+# GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
+#
+# while True: # Run forever
+#  GPIO.output(8, GPIO.HIGH) # Turn on
+#  sleep(1) # Sleep for 1 second
+#  GPIO.output(8, GPIO.LOW) # Turn off
+#  sleep(1) # Sleep for 1 second
