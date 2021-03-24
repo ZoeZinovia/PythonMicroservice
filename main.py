573374a79cf2c7d2541f1e3051eddac72934f225
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-03-24 16:03:00 +0000

    Adding adafruit temp and humidity

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import Adafruit_DHT
 
 sensor = 22 # might need to be changed depending on the pi setup
 pin = 12 # might need to be changed depending on the pi setup
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
+print("trying..")
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)  # Not doing anything...
 print(humidity)
 print(temperature)
+
 # GPIO.setwarnings(False) # Ignore warning for now
 # GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 # GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
