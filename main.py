153496b2f3dc034aad74644e400bbf8b867e37a0
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-03-24 16:42:03 +0000

    Adding adafruit temp and humidity

--- main.py
+++ main.py
@@ -1,29 +1,87 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep # Import the sleep function from the time module
-import Adafruit_DHT
+import RPi.GPIO as GPIO
 import time
 
-DHT_SENSOR = Adafruit_DHT.DHT11 # might need to be changed depending on the pi setup
-DHT_PIN = 4 # might need to be changed depending on the pi setup
-# print("trying..")
-# humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)  # Not doing anything...
-# print(humidity)
-# print(temperature)
+def bin2dec(string_num):
+    return str(int(string_num, 2))
 
-while True:
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}C  Humidity={1:0.1f}%".format(temperature, humidity))
-    else:
-        print("Sensor failure. Check wiring.");
-    time.sleep(3);
+data = []
 
-# GPIO.setwarnings(False) # Ignore warning for now
-# GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-# GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-#
-# while True: # Run forever
-#  GPIO.output(8, GPIO.HIGH) # Turn on
-#  sleep(1) # Sleep for 1 second
-#  GPIO.output(8, GPIO.LOW) # Turn off
-#  sleep(1) # Sleep for 1 second
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4,GPIO.OUT)
+GPIO.output(4,GPIO.HIGH)
+time.sleep(0.025)
+GPIO.output(4,GPIO.LOW)
+time.sleep(0.02)
+
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+for i in range(0,500):
+    data.append(GPIO.input(4))
+
+bit_count = 0
+tmp = 0
+count = 0
+HumidityBit = ""
+TemperatureBit = ""
+crc = ""
+
+try:
+	while data[count] == 1:
+		tmp = 1
+		count = count + 1
+
+	for i in range(0, 32):
+		bit_count = 0
+
+		while data[count] == 0:
+			tmp = 1
+			count = count + 1
+
+		while data[count] == 1:
+			bit_count = bit_count + 1
+			count = count + 1
+
+		if bit_count > 3:
+			if i>=0 and i<8:
+				HumidityBit = HumidityBit + "1"
+			if i>=16 and i<24:
+				TemperatureBit = TemperatureBit + "1"
+		else:
+			if i>=0 and i<8:
+				HumidityBit = HumidityBit + "0"
+			if i>=16 and i<24:
+				TemperatureBit = TemperatureBit + "0"
+
+except:
+	print ("ERR_RANGE")
+	exit(0)
+
+try:
+	for i in range(0, 8):
+		bit_count = 0
+
+		while data[count] == 0:
+			tmp = 1
+			count = count + 1
+
+		while data[count] == 1:
+			bit_count = bit_count + 1
+			count = count + 1
+
+		if bit_count > 3:
+			crc = crc + "1"
+		else:
+			crc = crc + "0"
+except:
+	print ("ERR_RANGE")
+	exit(0)
+
+Humidity = bin2dec(HumidityBit)
+Temperature = bin2dec(TemperatureBit)
+
+if int(Humidity) + int(Temperature) - int(bin2dec(crc)) == 0:
+	print (Humidity)
+	print (Temperature)
+else:
+	print ("ERR_CRC")
