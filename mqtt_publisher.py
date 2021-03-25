6cb94d6c9428fb6794b9de24564d83117ae9bdb1
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-03-25 12:38:58 +0000

    Updates with MQTT

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -7,21 +7,20 @@ import json
 MQTT_SERVER = "192.168.1.210"
 MQTT_PATH = "Temperature"
 
-publish.single(MQTT_PATH, "Hello there!! I'm connected :)", hostname=MQTT_SERVER)
+#Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D4)
 
+count = 0
+while count<20:
+    try:
+         # Print the values to the serial port
+         temperature = dhtDevice.temperature
+         humidity = dhtDevice.humidity
+         message = str("Temp: {:.1f} C    Humidity: {}% "
+               .format(temperature, humidity))
+         publish.single(MQTT_PATH, message, hostname=MQTT_SERVER)
 
-# #Initial the dht device, with data pin connected to:
-# dhtDevice = adafruit_dht.DHT11(board.D4)
-#
-# count = 0
-# while count<20:
-#     try:
-#          # Print the values to the serial port
-#          temperature = dhtDevice.temperature
-#          humidity = dhtDevice.humidity
-#          print("Temp: {:.1f} C    Humidity: {}% "
-#                .format(temperature, humidity))
-#
-#     except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
-#          print(error.args[0])
-#     count += 1
+    except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
+         print(error.args[0])
+    count += 1
+    time.sleep(5)
