0858b733b66f79e1f8a31dfb78f1d094afc222fe
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-03-25 14:57:41 +0000

    Updates with MQTT

--- humidityMicroservicePublisher.py
+++ humidityMicroservicePublisher.py
@@ -5,7 +5,7 @@ import paho.mqtt.publish as publish
 import json
 
 MQTT_SERVER = "192.168.1.210"
-MQTT_PATH = "Temperature"
+MQTT_PATH = "Humidity"
 
 #Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT11(board.D4)
@@ -16,10 +16,8 @@ while count<20:
          # Print the values to the serial port
          temperature = dhtDevice.temperature
          humidity = dhtDevice.humidity
-         temp_json = {"Temp": temperature}
-         # message = str("Temp: {:.2f}"
-         #       .format(temperature))
-         publish.single(MQTT_PATH, json.dumps(temp_json), hostname=MQTT_SERVER)
+         hum_json = {"Humidity": humidity, "Unit":"%"}
+         publish.single(MQTT_PATH, json.dumps(hum_json), hostname=MQTT_SERVER)
 
     except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
          print(error.args[0])
---
+++ temperatureMicroservicePublisher.py
@@ -0,0 +1,27 @@
+import time
+import board
+import adafruit_dht
+import paho.mqtt.publish as publish
+import json
+
+MQTT_SERVER = "192.168.1.210"
+MQTT_PATH = "Temperature"
+
+#Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D4)
+
+count = 0
+while count<20:
+    try:
+         # Print the values to the serial port
+         temperature = dhtDevice.temperature
+         humidity = dhtDevice.humidity
+         temp_json = {"Temp": temperature, "Unit":"C"}
+         publish.single(MQTT_PATH, json.dumps(temp_json), hostname=MQTT_SERVER)
+
+    except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
+         print(error.args[0])
+    count += 1
+    time.sleep(5)
+
+    #test
