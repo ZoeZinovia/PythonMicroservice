c15500a9c11c247f766919d880b05d1092686f0e
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-04-12 09:52:59 +0100

    Updates to humidity microservice

--- humidityMicroservicePublisher.py
+++ humidityMicroservicePublisher.py
@@ -7,21 +7,18 @@ import json
 MQTT_SERVER = "192.168.1.210"
 MQTT_PATH = "Humidity"
 
-#Initial the dht device, with data pin connected to:
+# Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT11(board.D4)
 
 count = 0
-while count<20:
+while count < 20:
     try:
-         # Print the values to the serial port
-         temperature = dhtDevice.temperature
-         humidity = dhtDevice.humidity
-         hum_json = {"Humidity": humidity, "Unit":"%"}
-         publish.single(MQTT_PATH, json.dumps(hum_json), hostname=MQTT_SERVER)
+        # Print the values to the serial port
+        humidity = dhtDevice.humidity
+        hum_json = {"Humidity": humidity, "Unit": "%"}
+        publish.single(MQTT_PATH, json.dumps(hum_json), hostname=MQTT_SERVER)
 
-    except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
-         print(error.args[0])
+    except RuntimeError as error:  # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
     count += 1
     time.sleep(5)
-
-    #test
