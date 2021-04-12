7197f00e5f81d7b0e4e7931c7ee016a06bbb37d6
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-04-12 17:26:03 +0100

    Updates to multiple published topics

--- temperatureMicroservicePublisher.py
+++ temperatureMicroservicePublisher.py
@@ -9,19 +9,20 @@ MQTT_PATH = "Temperature"
 
 #Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT11(board.D4)
+temperature = 0
 
-# count = 0
-# while count<20:
-#     try:
- # Print the values to the serial port
-temperature = dhtDevice.temperature
-humidity = dhtDevice.humidity
-temp_json = {"Temp": temperature, "Unit":"C"}
-publish.single(MQTT_PATH, json.dumps(temp_json), port=1883, hostname=MQTT_SERVER)
+count = 0
+while count<20:
+    try:
+         # Print the values to the serial port
+         newTemperature = dhtDevice.temperature
+         if newTemperature != temperature:
+             temp_json = {"Temp": temperature, "Unit":"C"}
+             publish.single(MQTT_PATH, json.dumps(temp_json), port=1883, hostname=MQTT_SERVER)
 
-    # except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
-    #      print(error.args[0])
-    # count += 1
-    # time.sleep(5)
+    except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
+         print(error.args[0])
+    count += 1
+    time.sleep(5)
 
     #test
