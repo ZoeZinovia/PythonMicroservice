9e6052bc5194f61bba179d25ee6b7ad139ee17dd
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-04-12 17:20:08 +0100

    Updates to humidity microservice

--- humidityMicroservicePublisher.py
+++ humidityMicroservicePublisher.py
@@ -10,15 +10,15 @@ MQTT_PATH = "Humidity"
 # Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT11(board.D4)
 
-count = 0
-while count < 20:
-    try:
-        # Print the values to the serial port
-        humidity = dhtDevice.humidity
-        hum_json = {"Humidity": humidity, "Unit": "%"}
-        publish.single(MQTT_PATH, json.dumps(hum_json), hostname=MQTT_SERVER)
+# count = 0
+# while count < 20:
+#     try:
+#         # Print the values to the serial port
+humidity = dhtDevice.humidity
+hum_json = {"Humidity": humidity, "Unit": "%"}
+publish.single(MQTT_PATH, json.dumps(hum_json), hostname=MQTT_SERVER)
 
-    except RuntimeError as error:  # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-    count += 1
-    time.sleep(5)
+    # except RuntimeError as error:  # Errors happen fairly often, DHT's are hard to read, just keep going
+    #     print(error.args[0])
+    # count += 1
+    # time.sleep(5)
--- temperatureMicroservicePublisher.py
+++ temperatureMicroservicePublisher.py
@@ -10,18 +10,18 @@ MQTT_PATH = "Temperature"
 #Initial the dht device, with data pin connected to:
 dhtDevice = adafruit_dht.DHT11(board.D4)
 
-count = 0
-while count<20:
-    try:
-         # Print the values to the serial port
-         temperature = dhtDevice.temperature
-         humidity = dhtDevice.humidity
-         temp_json = {"Temp": temperature, "Unit":"C"}
-         publish.single(MQTT_PATH, json.dumps(temp_json), port=1883, hostname=MQTT_SERVER)
+# count = 0
+# while count<20:
+#     try:
+ # Print the values to the serial port
+ temperature = dhtDevice.temperature
+ humidity = dhtDevice.humidity
+ temp_json = {"Temp": temperature, "Unit":"C"}
+ publish.single(MQTT_PATH, json.dumps(temp_json), port=1883, hostname=MQTT_SERVER)
 
-    except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
-         print(error.args[0])
-    count += 1
-    time.sleep(5)
+    # except RuntimeError as error:     # Errors happen fairly often, DHT's are hard to read, just keep going
+    #      print(error.args[0])
+    # count += 1
+    # time.sleep(5)
 
     #test
