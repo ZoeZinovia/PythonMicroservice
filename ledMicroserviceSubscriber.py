50a507fbc24205e6a8e2293a51fa68af1c95be29
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-04-14 11:00:55 +0100

    added functionality to end connection and get time

--- ledMicroserviceSubscriber.py
+++ ledMicroserviceSubscriber.py
@@ -7,6 +7,7 @@ MQTT_SERVER = "192.168.1.210"
 MQTT_PATH = "LED"
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+start = time.time()
 
 def on_connect(client, userdata, flags, rc):
     print("Connected. Result code: "+ str(rc))
@@ -18,18 +19,24 @@ def on_message(client, userdata, msg):
     msg.payload = msg.payload.decode("utf-8")
     print("message received: " + msg.payload)
     received_json = json.loads(msg.payload) #convert the string to json object
-    led_1_status = received_json["LED_1"]
-    led_1_gpio = received_json["GPIO"]
-    GPIO.setup(led_1_gpio, GPIO.OUT)
-    if led_1_status:
-        GPIO.output(led_1_gpio, GPIO.HIGH)
+    if "Done" in received_json:
+        client.disconnect()
+        end = time.time()
+        print("led subscriber connection closing. Total time running: " + end)
     else:
-        GPIO.output(led_1_gpio, GPIO.LOW)
-    print("Pi LED updated")
+        led_1_status = received_json["LED_1"]
+        led_1_gpio = received_json["GPIO"]
+        GPIO.setup(led_1_gpio, GPIO.OUT)
+        if led_1_status:
+            GPIO.output(led_1_gpio, GPIO.HIGH)
+        else:
+            GPIO.output(led_1_gpio, GPIO.LOW)
+        print("Pi LED updated")
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+
 
 client.connect(MQTT_SERVER, 1883, 60)
 client.loop_forever()
