400a0f8450edc06af99a5a6d3c6e8ae24a012a65
Zoe Zinovia <53115380+ZoeZinovia@users.noreply.github.com>
2021-05-17 16:54:08 +0100

    changing way dht readings are made

--- humidityTemperatureMicroservicePublisher.py
+++ humidityTemperatureMicroservicePublisher.py
@@ -6,6 +6,10 @@ import json
 import sys
 
 start = time.time()
+startDht = time.time()
+endDht = time.time()
+humidity = 0
+temperature = 0
 
 MQTT_SERVER = sys.argv[1]
 
@@ -14,8 +18,11 @@ dhtDevice = adafruit_dht.DHT11(board.D4)
 count = 0
 while count < 10000:
     try:
-        humidity = dhtDevice.humidity # Get current humidity from dht11
-        temperature = dhtDevice.temperature # Get current temperature from dht11
+        endDht = time.time()
+        if(humidity == 0 and temperature == 0) or (endDht-startDht) > 1:
+            humidity = dhtDevice.humidity # Get current humidity from dht11
+            temperature = dhtDevice.temperature # Get current temperature from dht11
+            startDht = time.time()
         hum_json = {"Humidity": humidity, "Unit": "%"}
         publish.single("Humidity", json.dumps(hum_json), hostname=MQTT_SERVER)
         temp_json = {"Temp": temperature, "Unit": "C"}
